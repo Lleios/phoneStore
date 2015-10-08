1726aef9317d7a2757c8f1617b0c3caa8643791e
Youri Chcouropat <youri.chcouropat@etu.univ-nantes.fr>
2015-10-08 11:20:13 +0200

    jeu de test

--- phoneStore/Magasins.java
+++ phoneStore/Magasins.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
 /**
 * Classe Magasin
 *
@@ -6,13 +9,21 @@
 
 public class Magasins {
 
-	/**
+	/** Jeu de test pour les classes crée
 	 * 
 	 * @param args
 	 */
 	public void main(String[] args) {
-		// TODO - implement Magasins.main
-		throw new UnsupportedOperationException();
+		ListeArticles listeArticles = new ListeArticles();
+		
+		
+		Coque co1 = new Coque(17256, "Coque", (float) 12.90, 
+				new HashSet<String>(Arrays.asList(new String[] {"Iphone 4", "Iphone 4S"})),
+				Couleur.rose);
+		
+		listeArticles.add(co1);
+		
+		listeArticles.afficher();
 	}
 
 }
